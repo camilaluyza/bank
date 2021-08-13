f0f78c76df070846399add17a47c18db61085057
Camila Luiza <78321076+camilaluyza@users.noreply.github.com>
2021-08-12 21:00:54 -0300

    Update index.js
    inserindo atributos

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
+    rg;
 }
 
- const cliente1 = new Cliente();
- const cliente2 = new Cliente();
+class ContaCorrente {
+    agencia;
+    saldo;
 
- cliente1.nome = "Camila";
- cliente1.cpf = 12581757545;
- cliente1.agencia = 0001;
- cliente1.saldo = 0;
-
- cliente2.nome = "Luiza";
- cliente2.cpf = 12544757545;
- cliente2.agencia = 0001;
- cliente2.saldo = 0;
-
+    sacar(valor){
+    let valorSacado = 200;
+    if(this.saldo >= valor){
+       this.saldo -= valor;
+       }
+    }
+}
 
 
+    const cliente1 = new Cliente();
+    cliente1.nome = "Camila";
+    cliente1.cpf = 12581757545;
+    cliente1.rg = 125478441;
 
-console.log(cliente1, cliente2);
+    const cliente2 = new Cliente();
+    cliente2.nome = "Luiza";
+    cliente2.cpf = 12544757545;
+    cliente2.rg = 213494566;
+
+const contaCorrenteCamila = new ContaCorrente();
+contaCorrenteCamila.saldo = 0;
+contaCorrenteCamila.agencia = 0001;
+
+console.log(contaCorrenteCamila.saldo);
+contaCorrenteCamila.saldo = 100;
+console.log(contaCorrenteCamila.saldo);
+contaCorrenteCamila.sacar(50);
+
+contaCorrenteCamila.saldo -= valorSacado;
+
+console.log(contaCorrenteCamila.saldo);
+console.log(cliente1);
+console.log(cliente2);
